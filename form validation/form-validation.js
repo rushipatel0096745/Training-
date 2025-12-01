832cde129c773c4d5501aeeb42816dffcf7c6059
rushipatel0096745 <rp0096745@gmail.com>
2025-12-01 17:16:50 +0530

    Add files via upload

--- form validation/form-validation.js
+++ form validation/form-validation.js
@@ -1,121 +1,131 @@
+function findNextEle(startElement) {
+  let current = startElement.nextElementSibling;
+
+  while (current) {
+    if (current.tagName == "DIV") {
+      return current;
+    }
+    current = current.nextElementSibling;
+  }
+  return null;
+}
+
+function getCharCounter(event, ele) {
+  event.preventDefault();
+  const text = ele.value;
+  // const counterEle = document.getElementById("counter")
+  // counterEle.textContent = `Character count is ${text.length}`
+  const nextEle = ele.nextElementSibling;
+  nextEle.textContent = `Character count is ${text.length}`;
+}
+
 function submitHandler(event) {
   event.preventDefault();
-  const form = document.querySelectorAll("form");
-  const displayError = document.querySelector(".errors");
-  // console.log(form);
-  // console.log(form[0]);
-  // console.log(form[0].length);
-  const nameError = document.querySelector(".nameError")
-  const dobError = document.querySelector(".dobError")
-  const emailError = document.querySelector(".emailError")
-  const addressError = document.querySelector(".addressError")
-  const mobileError = document.querySelector(".mobileError")
-  const genderError = document.querySelector(".genderError")
-  const eduError = document.querySelector(".eduError")
-  const termError = document.querySelector(".termError")
+  const submittedForm = event.target;
+  console.log(submittedForm);
 
-  const validationError = document.getElementsByClassName("validation-error")
-
-  let requiredFields = document.querySelectorAll(".required");
-  // console.log(requiredFields);
-  // console.log(requiredFields.length);
-
-  // displayError.textContent = "";
+  let requiredFields = submittedForm.querySelectorAll(".required");
   const radioNameSets = new Set();
   const checkBoxesSet = new Set();
 
-  let errors = [];
+  // let errors = [];
 
   for (let i = 0; i < requiredFields.length; i++) {
-
-
-
     // type email
     const emailRegex = /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/;
     if (requiredFields[i].type == "email") {
-      
-      
-      const nextEle = requiredFields[i].nextElementSibling
+      const nextEle = requiredFields[i].nextElementSibling;
       console.log(nextEle);
 
       if (requiredFields[i].value === "") {
         if (nextEle) {
-          nextEle.textContent = "email is required"
+          nextEle.textContent = "email is required";
         }
       } else if (!emailRegex.test(requiredFields[i].value)) {
         // errors.push("Email is invalid");
         if (nextEle) {
-          nextEle.textContent = "email is invaluid"
+          nextEle.textContent = "email is invalid";
         }
-        
+      } else {
+        nextEle.textContent = "";
       }
-      else {
-        nextEle.textContent = ""
-      }
-      
     }
-    
 
     // type text
     if (requiredFields[i].type == "text") {
-      const nextEle = requiredFields[i].nextElementSibling
+      const nextEle = requiredFields[i].nextElementSibling;
       console.log(nextEle);
 
       if (requiredFields[i].value === "") {
         // errors.push(`${requiredFields[i].name} is required`);
         // displayError.innerHTML = `${requiredFields[i].name} is required`
         // para.textContent = `${requiredFields[i].name} is required`
-        nextEle.textContent = `${requiredFields[i].name} is required`
-      }
-      else {
-        nextEle.textContent = ""
+        nextEle.textContent = `${requiredFields[i].name} is required`;
+      } else {
+        nextEle.textContent = "";
       }
     }
 
     // type date
     if (requiredFields[i].type == "date") {
       // const para = document.createElement('p')
-      const nextEle = requiredFields[i].nextElementSibling
+      const nextEle = requiredFields[i].nextElementSibling;
       console.log(nextEle);
 
       if (requiredFields[i].value === "") {
         // errors.push(`${requiredFields[i].name} is required`);
         // displayError.innerHTML = `${requiredFields[i].name} is required`
-        nextEle.textContent = `${requiredFields[i].name} is required`
+        nextEle.textContent = `${requiredFields[i].name} is required`;
       }
       // requiredFields[i].insertAdjacentElement("afterend", para)
       else {
-        nextEle.textContent = ""
+        nextEle.textContent = "";
       }
     }
 
     // type tel
     const mobRegex = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/;
     if (requiredFields[i].type == "tel") {
-      const nextEle = requiredFields[i].nextElementSibling
+      const nextEle = requiredFields[i].nextElementSibling;
       console.log(nextEle);
       // const para = document.createElement('p')
       if (requiredFields[i].value === "") {
         // errors.push(`${requiredFields[i].name} is required`);
         // displayError.innerHTML = `${requiredFields[i].name} is required`
-        nextEle.textContent = `${requiredFields[i].name} is required`
+        nextEle.textContent = `${requiredFields[i].name} is required`;
       } else if (!mobRegex.test(requiredFields[i].value)) {
         // errors.push(`${requiredFields[i].name} is required`);
-        nextEle.textContent = `${requiredFields[i].name} is invalid`
+        nextEle.textContent = `${requiredFields[i].name} is invalid`;
       }
       // requiredFields[i].insertAdjacentElement("afterend", para)
-      else{
-        nextEle.textContent = ""
+      else {
+        nextEle.textContent = "";
       }
+    }
+
+    // for selection
+    if (requiredFields[i].tagName == "SELECT") {
+      // const para = document.createElement('p')
+      const nextEle = requiredFields[i].nextElementSibling;
+
+      if (requiredFields[i].value === "") {
+        // errors.push(`select one of the option for ${requiredFields[i].name}`);
+        nextEle.textContent = `select one of the option for ${requiredFields[i].name}`;
+      } else {
+        nextEle.textContent = "";
+      }
+      // requiredFields[i].insertAdjacentElement("afterend", para)
     }
 
     // type radio
     if (requiredFields[i].type === "radio") {
-      // const para = document.createElement('p')
-      const nextEle = requiredFields[i].nextElementSibling
+      // const nextEle = requiredFields[i].nextElementSibling
+      const nextEle = findNextEle(requiredFields[i]);
+
       const totalRadios = document.querySelectorAll(
         'input.required[type="radio"]'
       );
+
       for (let i = 0; i < totalRadios.length; i++) {
         if (radioNameSets.has(totalRadios[i].name)) {
           continue;
@@ -134,104 +144,107 @@ function submitHandler(event) {
         // console.log(radioNameSets);
         if (!isChecked) {
           // errors.push(`select the ${totalRadios[i].name}`);
-          nextEle.textContent = `select the ${totalRadios[i].name}`
-        }
-        else {
-          nextEle.textContent = ""
+          nextEle.textContent = `select the ${totalRadios[i].name}`;
+        } else {
+          nextEle.textContent = "";
         }
       } //end of inner loop
       // requiredFields[i].insertAdjacentElement("afterend", para)
-
     }
-
-    // for selection
-    if (requiredFields[i].tagName == "SELECT") {
-      // const para = document.createElement('p')
-      const nextEle = requiredFields[i].nextElementSibling
-
-      if (requiredFields[i].value === "") {
-        // errors.push(`select one of the option for ${requiredFields[i].name}`);
-        nextEle.textContent = `select one of the option for ${requiredFields[i].name}`
-      }
-      else {
-        nextEle.textContent = ""
-      }
-      // requiredFields[i].insertAdjacentElement("afterend", para)
-    }
-
-    // console.log("outer loop iter", i, requiredFields[i].type);
-
-
+    console.log("outer i", i);
     // for checkboxes
     if (requiredFields[i].type === "checkbox") {
-      // const para = document.createElement('p')
-      const nextEle = requiredFields[i].nextElementSibling
-      
+      const nextEle = findNextEle(requiredFields[i]);
+      console.log(nextEle);
 
       let totalCheckboxes = document.querySelectorAll(
         'input.required[type="checkbox"]'
       );
-      // console.log("total checkboxes", totalCheckboxes);
-      // console.log("total checkboxes", totalCheckboxes.length);
+
+      console.log("total checkboxes", totalCheckboxes);
+      console.log("total checkboxes", totalCheckboxes.length);
 
       for (let i = 0; i < totalCheckboxes.length; i++) {
         _name = totalCheckboxes[i].name;
-        // console.log("name", _name);
+        console.log("name", _name);
         if (checkBoxesSet.has(totalCheckboxes[i].name)) {
           console.log("skipping this time");
           continue;
         }
-        // console.log("iteration", i);
+        console.log("iteration", i);
 
         console.log(checkBoxesSet.add(totalCheckboxes[i].name));
 
         let tempCheckboxes = document.getElementsByName(
           totalCheckboxes[i].name
         );
-        // console.log("temp checkboxes", tempCheckboxes);
-        // console.log("temp checkboxes", tempCheckboxes.length);
-
+        console.log("temp checkboxes", tempCheckboxes);
+        console.log("temp checkboxes", tempCheckboxes.length);
 
         // console.log(Array.from(tempCheckboxes).some((item) => item.checked));
+
+        // for (let i = 0; i < tempCheckboxes.length; i++) {
+        //   let notSelected = 0
+        //   if(!tempCheckboxes[i].checked){
+        //     notSelected += 1
+        //   }
+          
+        // }
 
         if (!Array.from(tempCheckboxes).some((item) => item.checked)) {
           console.log("pushing error");
           // errors.push(`please select the ${totalCheckboxes[i].name}`);
-          nextEle.textContent = `please select the ${totalCheckboxes[i].name}`
-        }
-        else {
-          nextEle.textContent = ""
+          nextEle.textContent = `please select the ${tempCheckboxes[i].name}`;
+        } else {
+          nextEle.textContent = "";
         }
       }
       // requiredFields[i].insertAdjacentElement("afterend", para)
     }
 
-    // for textarea
-    if (requiredFields[i].tagName == "TEXTAREA") {
-      // const para = document.createElement('p')
-      const nextEle = requiredFields[i].nextElementSibling
+    // for number
+    if (requiredFields[i].type == "number") {
+      const nextEle = requiredFields[i].nextElementSibling;
+      // console.log(nextEle);
 
       if (requiredFields[i].value === "") {
-        // errors.push(`${requiredFields[i].name} is required`);
-        nextEle.textContent = `${requiredFields[i].name} is required`
-      } 
-      else{
-        nextEle.textContent = ""
+        nextEle.textContent = `${requiredFields[i].name} is required`;
+      } else {
+        nextEle.textContent = "";
       }
-      
-      
+    }
+
+    // for textarea
+    if (requiredFields[i].tagName == "TEXTAREA") {
+      const nextEle = findNextEle(requiredFields[i]);
+
+      const text = requiredFields[i].value.trim();
+
+      const maxAttr = requiredFields[i].getAttribute("max");
+
+      if (requiredFields[i].value === "") {
+        nextEle.textContent = `${requiredFields[i].name} is required`;
+      } else if (maxAttr) {
+        if (text.length > maxAttr) {
+          nextEle.textContent = `no more than ${maxAttr} characters are allowed`;
+        } else {
+          nextEle.textContent = "";
+        }
+      } else {
+        nextEle.textContent = "";
+      }
+
+      // requiredFields[i].addEventListener("keyup", () => {
+      //   const text = requiredFields[i].value.trim();
+      //   console.log(` Character count: ${text.length}`);
+      //   nextEle.innerHTML = `<span style="color: black">
+      //   Character count: ${text.length}
+      //   </span>`;
+
+      // });
     }
   } //end of for loop
-  
-  // else {
-  //   requiredFields[i].nextElementSibling.remove()
-  // }
-  console.log("errors....", errors);
-  console.log("errors....", errors.length);
 
-  // for (let i = 0; i < errors.length; i++) {
-  //   displayError.innerHTML += `<ul>
-  //       <li>${errors[i]}</li>
-  //     </ul>`;
-  // }
+  // console.log("errors....", errors);
+  // console.log("errors....", errors.length);
 }
